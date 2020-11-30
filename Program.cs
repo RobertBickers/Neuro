094fc7fe2386c7fd6e048ca33cd68993f3e3e960
Robert Bickers <bickers.robert@gmail.com>
2020-11-30 15:41:39 +0000

    Fancy message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what's your name?");
+            var nameOfUsers = Console.ReadLine();
+            Console.WriteLine($"Excellent, thanks for that, {nameOfUsers}");            
+        }
+    }
+}

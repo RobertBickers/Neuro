ec28439e1da7371c2048c9a53a00cced617b5088
Robert Bickers <bickers.robert@gmail.com>
2020-11-30 15:45:40 +0000

    Added some context of what the user would like help with

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace consoleapp
         {
             Console.WriteLine("Hello, what's your name?");
             var nameOfUsers = Console.ReadLine();
-            Console.WriteLine($"Excellent, thanks for that, {nameOfUsers}");            
+            Console.WriteLine($"Excellent, thanks for that. From now on I'll call you {nameOfUsers}");            
+            Console.WriteLine("Is there anything I can help you with?");
+            var whatTheUserWouldLikeHelpWith = Console.ReadLine();
         }
     }
 }
